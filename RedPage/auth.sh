8338d6d31baa5d66e890493341e44b43dcd28b17
Ra1nB0w <65203923+sw1128@users.noreply.github.com>
2023-09-11 15:04:46 +0800

    Update auth.sh

--- RedPage/auth.sh
+++ RedPage/auth.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <StudentID> <Password>"
+  exit 1
+fi
+
+studentId="$1"
+studentPwd="$2"
+
+url="http://10.21.221.98:801/eportal/portal/login?callback=dr1003&login_method=1&user_account=${studentId}@campus&user_password=${studentPwd}"
+
+response=$(curl -s "$url")
+
+echo "Response: $response"
